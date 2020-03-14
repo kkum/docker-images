385c4d31ef69f1e39843d02fb20a80eaea099812
Kumar KIRPALANI <kumar.kirpalani@versusmind.eu>
2020-03-14 07:39:13 +0100

    Add push image file

--- Build-PushOnly.ps1
+++ Build-PushOnly.ps1
@@ -14,10 +14,16 @@ Get-Content $TagFile| ForEach-Object {
     $tag = $_ ;
     $repositoryTag = "$($Repository).azurecr.io/$($tag)" ;
     #Ensure login
-    az acr login --name $Repository ;
-    # tag the file fot repository
-    docker image tag $tag $repositoryTag;
+    $tagId = $(docker images "$tag" --format "{{.ID}}")
+    $repositoryTagId = $(docker images "$tag" --format "{{.ID}}")
+
+    # (re)tag the file for repository if necessary
+    if ( ($null -eq $repositoryTagId) -or ($tagId -ne $repositoryTagId)) {
+        docker image tag $tag $repositoryTag;
+    }
+
     # push the image
+    az acr login --name $Repository ;
     docker image push $repositoryTag;
 }
 if($RemoveFile.IsPresent){
